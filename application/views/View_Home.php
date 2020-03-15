954920a0123b1fccd1e6cab28f9b49138d669915
BrandonAndradeR <60976033+BrandonAndradeR@users.noreply.github.com>
2020-03-15 12:03:55 -0500

    Configuraciones

---
+++ application/views/View_Home.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <style type="text/css">
+    body {
+       background-image: url("http://localhost/Sitioweb/imagenes/fondo4.jpg");
+       
+        font-family: cursive;
+}
+  
+   small{
+
+    background-color: black;
+   }
+   header{
+
+     background-color: black;
+   }
+   h1{ 
+     
+     
+     color:  darkturquoise; }
+ 
+   
+</style>  
+
+<body>
+    <div class="container">
+    
+  <header class="blog-header py-1" >
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-2 pt-2">
+
+      </div>
+      <div class="col-6 text- left">
+        
+       <br> <a class="blog-header-logo text-darck" href="#"><h1 >TIENDA VIRTUAL FET</h1></a></br>
+        <h5>
+           <small class="text-muted">ESTAMOS PARA USTEDES</small>
+    </h5>
+      </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="text-acive" href="#" aria-label="Search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+        </a>
+        
+      </div>
+    </div>
+  </header>
+
+  <br><div class="nav-scroller py-1 mb-2">
+    <nav class="nav d-flex justify-content-between">
+     <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      CATEGORIAS
+      </button>
+       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="">COMPUTACION</a>
+            <a class="dropdown-item" href="#">PRODUCTOS DEL HOGAR</a>
+            <a class="dropdown-item" href="#">CELULARES</a>
+        </div>
+     </div>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-secondary">servicios</button>
+      <button type="button" class="btn btn-secondary">mi carrito</button>
+
+    </nav>
+    </div></br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="myCarousel" data-slide-to="1"></li>
+        <li data-target="myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="myCarousel" data-slide-to="1"></li>
+        <li data-target="myCarousel" data-slide-to="2"></li>
+        <li data-target="myCarousel" data-slide-to="3"></li>
+        <li data-target="myCarousel" data-slide-to="4"></li>
+        <li data-target="myCarousel" data-slide-to="5"></li>
+    </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+
+      <img src="http://localhost/sitioweb/imagenes/gamer.jpg" class="d-block w-100"  width: 100px; height: 100px>
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost/sitioweb/imagenes/electro.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img  src="http://localhost/sitioweb/imagenes/tele.jpg" class="d-block w-100" width: 200px; height: 200px>
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost/sitioweb/imagenes/tele.jpg" class="d-block w-100" width: 200px; height: 200px>
+    </div>
+    <div class="carousel-item">
+      <img  src="http://localhost/sitioweb/imagenes/tele.jpg" class="d-block w-100" width: 200px; height: 200px>
+    </div>
+    <div class="carousel-item">
+      <img  src="http://localhost/sitioweb/imagenes/tele.jpg" class="d-block w-100" width: 200px; height: 200px>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+<br><center><h5>TELEFONOS</h5></center></br>
+  <br><div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-290 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">Samsung</strong>
+          <h3 class="mb-0">Caracteristicas</h3>
+          <div class="mb-1 text-muted"></div>
+          <p class="card-text mb-auto"></p>
+          <a href="#" class="stretched-link"></a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">hola</strong>
+          <h3 class="mb-0"></h3>
+          <div class="mb-1 text-muted">Nov 11</div>
+          <p class="mb-auto"></p>
+          <a href="#" class="stretched-link">CELULARES</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-290 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">Samsung</strong>
+          <h3 class="mb-0">Caracteristicas</h3>
+          <div class="mb-1 text-muted"></div>
+          <p class="card-text mb-auto"></p>
+          <a href="#" class="stretched-link"></a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">hola</strong>
+          <h3 class="mb-0"></h3>
+          <div class="mb-1 text-muted">Nov 11</div>
+          <p class="mb-auto"></p>
+          <a href="#" class="stretched-link">CELULARES</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+     
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill= "http://localhost/proyecto/imagenes/5c6c67351c95f-256x256.jpeg"/></svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+e="main" class="container">
+  <div class="row">
+    <div class="col-md-8 blog-main">
+      <h3 class="pb-4 mb-4 font-italic border-bottom">
+        From the Firehose
+      </h3>
+
+      
+<center>
+      <div class="p-4">
+        <h4 class="font-italic">Elsewhere</h4>
+        <ol class="list-unstyled">
+          <li><a href="#">GitHub</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ol>
+      </div>
+</center>
+    </aside><!-- /.blog-sidebar -->
+
+  </div><!-- /.row -->
+
+ </main><!-- /.container -->
+
+ <footer class="blog-footer">
+  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+ /footer>
+</body>
+</html>
